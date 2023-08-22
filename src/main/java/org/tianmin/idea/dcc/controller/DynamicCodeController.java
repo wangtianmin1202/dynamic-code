fe1ac554125e9bc433fd6e8a440c644d6ec0ae81
wangtianmin <wangtianmin1202@163.com>
2023-08-22 21:38:34 +0800

    push

--- src/main/java/org/tianmin/idea/dcc/controller/DynamicCodeController.java
+++ src/main/java/org/tianmin/idea/dcc/controller/DynamicCodeController.java
@@ -1,6 +1,9 @@
 package org.tianmin.idea.dcc.controller;
 
 import groovy.lang.GroovyClassLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dynamic-code")
 public class DynamicCodeController {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/get-method")
     public ResponseEntity<?> getMethod(String description) {
+        String dccValue = stringRedisTemplate.opsForValue().get("dcc:");
         return ResponseEntity.ok("description");
     }
 }
